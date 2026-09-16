80d5baaec6e81b5220d94ce2466e04cdbf788d7a
Tayrick LOUIS-CHARLES <118570701+oshiroJBS@users.noreply.github.com>
2026-09-16 21:41:46 +0200

    Update gameTest.jsx

--- gameTest.jsx
+++ gameTest.jsx
@@ -1,5 +1,5 @@
 async function BounceCristals() {
     await functions.moveCards(cards.Cristals, "Hand")
     await functions.updateCards(cards.Hand, { isHidden: "opponent-only" })
-    "window.print()">bounce
+    console.log("bounced)
 }
